0f1c71be4f8f93aa940fbcedad386adcaa1037ac
David <david.langston26@outlook.com>
2019-12-03 09:29:58 -0500

    Solved Part 1
    Moved file parsing into separate function; added try block to dump state at undefined opCode

--- Day-02/part-1.py
+++ Day-02/part-1.py
@@ -2,6 +2,18 @@
 
 import csv
 
+def parseFile(inFile):
+  progs = []
+  
+  with open(inFile, 'r') as f:
+    reader = csv.reader(f)
+    progs = list(reader)
+
+  for prog in progs:
+    for i in range(len(prog)):
+      prog[i] = int(prog[i])
+
+
 def runTest(prog):
   print(prog)
   runOpCode(prog)
@@ -24,7 +36,7 @@ def getOpr(opCode):
     return ops[opCode]
   else:
     print("Illigal OpCode")
-    exit(-1)
+    return -1
 
 def runOpCode(program):
   pos = 0
@@ -45,25 +57,21 @@ def runOpCode(program):
 
     #printState(opr, d1, d2, out)    
 
-    program[out] = opr(d1, d2)
+    try:
+      program[out] = opr(d1, d2)
+    except:
+      print(f"Dumpting Program State\n\n{program}\n]n")
+      exit(-1)
+
     pos += 4
 
 
-#test_prog = [1,0,0,0,99]
+#Original Input
+#flightProg = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0]
 
-#runTest(test_prog)
+#Modified Input
+flightProg = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0]
 
-inputs = 'test-input'
-
-with open(inputs, 'r') as f:
-  reader = csv.reader(f)
-  progs = list(reader)
-
-for prog in progs:
-  for i in range(len(prog)):
-    prog[i] = int(prog[i])
-
-for prog in progs:
-  print(f"In : {prog}")
-  runOpCode(prog)
-  print(f"Out: {prog}")
+#print(f"In : {flightProg}")
+runOpCode(flightProg)
+print(f"Out: {flightProg}")
